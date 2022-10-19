7b627e94714d8b54ce5b8d1c7ed07a464ad05e4c
Ahmad Junaidisk <94750834+AhmadJunaidisk@users.noreply.github.com>
2022-10-19 20:23:18 +0700

    Update crud_query.php
    - menambahkan method INSERT, UPDATE(Berdasarkan WHERE)
    - menambahkan method get( ), fungsinya untuk mem-fetch data(Hanya 1 data)
    - menambahkan method getall( ), fungsinya untuk mem-fetch data(Semua data)

--- crud_query.php
+++ crud_query.php
@@ -4,45 +4,52 @@ require_once "koneksi.php";
 
 class Query extends DB {
 
-    /**
-     * variabel static ini berisi nama tabel
-     * @return string
-     */
-    private static $tb;
-
-    /**
-     * variabel ini berisi statement query yang akan
-     * dieksekusi di fungsi exec()
-     * @return void
-     */
-    private $query_stmt;
-
-    /** 
-     * variabel ini berisi key dari array
-     * @return string
-     */
-    private $key;
-
-    /**
-     * variabel ini berisi kode untuk "mentrigger" fungsi exec()
-     * @return int
-     */
+    private $stmt_query;
     private $trigger_code;
+    private static $tb;
+    private static $klausa = [];
+    private $key;
+    private $key_execute;
+    private $update_param = [];
+    private $insert_update_trigger = 0;
 
     public static function table(string $arg) {
         self::$tb = $arg;
         return new static;
     }
 
+    public function setQuery(string $query) {
+        $this->stmt_query .= $query;
+    }
+
+    public function select($kolom) {
+        $nama_kolom = "";
+        if(is_array($kolom)) {
+            foreach($kolom as $q) {
+                if($q != $kolom[array_key_last($kolom)]) {
+                    $nama_kolom .= $q.",";
+                } else {
+                    $nama_kolom .= $q;
+                }
+            }
+            self::setQuery("SELECT $nama_kolom FROM ".self::$tb);
+        } else if($kolom == "*") {
+            self::setQuery("SELECT * FROM ".self::$tb);
+        }
+        $this->trigger_code = "__SELECT__";
+        return $this;
+    }
+
     public function insert(array $column, array $value) {
-        $column_stmt = "";
+        $tanda = "";
         $prepare = "";
+        $this->iu_update = $value;
         if(is_array($column) && is_array($value)) {
             foreach($column as $clm) {
                 if($clm != $column[array_key_last($column)]) {
-                    $column_stmt .= $clm.",";
+                    $tanda .= $clm.",";
                 } else {
-                    $column_stmt .= $clm;
+                    $tanda .= $clm;
                 }
             }   
             for($i=1; $i<count($value)+1; $i++) {
@@ -52,73 +59,134 @@ class Query extends DB {
                     $prepare .= "?";
                 }
             }
-            $query = "INSERT INTO ".self::$tb." (".$column_stmt.") VALUES (".$prepare.")";
-            $stmt = $this->koneksi->prepare($query);
-            $stmt->execute($value); 
+            self::setQuery("INSERT INTO ".self::$tb." (".$tanda.") VALUES (".$prepare.")");
+            $this->insert_update_trigger = 1;
+            return $this;
         }
     }
 
-    public function setQuery(string $query) {
-        $this->query_stmt .= $query;
-    }
-
-    public function select(array $column) {
-        $column_stmt = "";
-        if(is_array($column)) {
-            foreach($column as $q) {
-                if($q != $column[array_key_last($column)]) {
-                    $column_stmt .= $q.",";
+    public function update(array $column, array $value) {
+        $tanda = "";
+        $prepare = "";
+        $loop = -1;
+        if(is_array($column) && is_array($value)) {
+            foreach($column as $clm) {
+                $loop += 1;
+                if($clm != $column[array_key_last($column)]) {
+                    $tanda .= $clm." = ? ,";
                 } else {
-                    $column_stmt .= $q;
+                    $tanda .= $clm." = ?";
                 }
-            }
-            $this->trigger_code = 0;
-            self::setQuery("SELECT $column_stmt FROM ".self::$tb);
+                $this->update_param[] .= $value[$loop];
+            }   
+            self::setQuery("UPDATE ".self::$tb." SET ".$tanda);
+            $this->insert_update_trigger = 2;
             return $this;
         }
     }
 
     public function where(array $arg) {
-        if(is_array($arg)) {
+        if(is_array($arg)) {        
             $this->key = $arg[array_key_first($arg)];
             self::setQuery(" WHERE ".array_key_first($arg). " = ?");
-            $this->trigger_code = 1;
-            return $this;
+            self::$klausa[] = $arg;
+            if($this->insert_update_trigger == 0) {
+                $this->trigger_code = "__WHERE__";
+            } else if($this->insert_update_trigger == 1){
+                $this->trigger_code = "__INSERT__";
+            } else if($this->insert_update_trigger == 2){
+                $this->trigger_code = "__UPDATE__";
+                $this->update_param[] = $this->key;
+            }
         }
+        return $this;
+    }
+
+    public function push() {
+        $stmt = $this->koneksi->prepare($this->stmt_query);
+        if($this->trigger_code == "__INSERT__") {
+            $stmt->execute($this->update_param); 
+        } else if($this->trigger_code == "__UPDATE__") {
+            $stmt->execute($this->update_param); 
+        }
+    }
+
+    public function or(array $arg) {
+        if(is_array($arg)) {
+            self::setQuery(" OR ".array_key_first($arg). " = ?");
+            self::$klausa[] = $arg;
+            $this->trigger_code = "__OR__";
+        }
+        return $this;
     }
 
     public function like(array $arg) {
         if(is_array($arg)) {
             $this->key = $arg[array_key_first($arg)];
             self::setQuery(" WHERE ".array_key_first($arg). " LIKE ?");
-            $this->trigger_code = 2;
+            $this->trigger_code = "__LIKE__";
             return $this;
         }
     }
 
-    public function exec() {
-        $stmt = $this->koneksi->prepare($this->query_stmt);
-        if($this->trigger_code == 1) {
-            $stmt->execute([$this->key]);
-            return $stmt->fetch(PDO::FETCH_ASSOC);
-        } else if($this->trigger_code == 2) {
-            $stmt->execute(["%".$this->key."%"]);
-            return $stmt->fetchall(PDO::FETCH_ASSOC);
-        } else if($this->trigger_code == 0) {
-            $stmt = $this->koneksi->prepare($this->query_stmt);
+    public function get() {
+        $stmt = $this->koneksi->prepare($this->stmt_query);
+        if($this->trigger_code == "__SELECT__") {
             $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else if($this->trigger_code == "__WHERE__") {
+            $stmt->execute([$this->key]);
+        } else if($this->trigger_code == "__OR__") {
+            foreach(self::$klausa as $clause_key) {
+                $this->key_execute[] .= implode($clause_key)." ";
+            }
+            $stmt->execute($this->key_execute);
+        } else if($this->trigger_code == "__LIKE__") {
+            return $this->getall();
+        }
+        if($stmt->rowcount() != 0) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return json_encode(["data" => ["status" => 400, "message" => "data kosong"]], JSON_PRETTY_PRINT);
+        }
+    }
+
+    public function getall() {
+        $stmt = $this->koneksi->prepare($this->stmt_query);
+        if($this->trigger_code == "__SELECT__") {
+            $stmt->execute();
+        } else if($this->trigger_code == "__WHERE__") {
+            $stmt->execute([$this->key]);
+        } else if($this->trigger_code == "__OR__") {
+            foreach(self::$klausa as $clause_key) {
+                $this->key_execute[] .= implode($clause_key)." ";
+            }
+            $stmt->execute($this->key_execute);
+        } else if($this->trigger_code == "__LIKE__") {
+            $stmt->execute(["%".$this->key."%"]);
+        } 
+        if($stmt->rowcount() != 0) {
+            return $stmt->fetchall(PDO::FETCH_ASSOC);
+        } else {
+            return json_encode(["data" => ["status" => 400, "message" => "data kosong"]], JSON_PRETTY_PRINT);
         }
     }
 
     public function numrows() {
-        $stmt = $this->koneksi->prepare($this->query_stmt);
-        if($this->trigger_code == 1) {
+        $stmt = $this->koneksi->prepare($this->stmt_query);
+        if($this->trigger_code == "__SELECT__") {
+            $stmt->execute();
+        } else if($this->trigger_code == "__WHERE__") {
             $stmt->execute([$this->key]);
-        } else if($this->trigger_code == 2) {
+        } else if($this->trigger_code == "__OR__") {
+            foreach(self::$klausa as $clause_key) {
+                $this->key_execute[] .= implode($clause_key)." ";
+            }
+            $stmt->execute($this->key_execute);
+        } else if($this->trigger_code == "__LIKE__") {
             $stmt->execute(["%".$this->key."%"]);
-        }
+        } 
         return $stmt->rowCount();
     }
- 
 }
+
+?>
